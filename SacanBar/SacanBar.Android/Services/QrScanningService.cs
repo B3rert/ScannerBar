62252a9d376d69e9db416797dcc279d035523eb3
Robert L <robertmorales181@gmail.com>
2020-12-22 18:09:39 -0600

    Todos los cambios

---
+++ SacanBar/SacanBar.Android/Services/QrScanningService.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SacanBar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing.Mobile;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SacanBar.Droid.Services.QrScanningService))]
+
+namespace SacanBar.Droid.Services
+{
+    public class QrScanningService : IQrScanningService
+    {
+
+        public async Task<string> ScanAsync()
+        {
+            var optionDefault = new MobileBarcodeScanningOptions();
+            var optionCustom = new MobileBarcodeScanningOptions();
+
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "",
+                BottomText = ""
+                
+            };
+
+            var scanResult = await scanner.Scan(optionCustom);
+            return scanResult.Text;
+        }
+
+    }
+}
---
+++ SacanBar/SacanBar/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SacanBar.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SacanBar
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async  void btnScan_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+                var scanner = DependencyService.Get<IQrScanningService>();
+                var result = await scanner.ScanAsync();
+                if (result != null)
+                {
+                    txtBarCode.Text = result;
+                }
+            }catch(Exception ex)
+            {
+                throw;
+            }
+            
+
+        }
+    }
+}
